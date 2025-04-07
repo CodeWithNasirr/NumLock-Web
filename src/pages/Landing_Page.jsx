d210600baf8b835381f574fd1b51e5b0db23309b
CodeWithNasirr <skofficial665@gmail.com>
2025-04-07 23:47:04 +0530

    Seo DOne

--- src/pages/Landing_Page.jsx
+++ src/pages/Landing_Page.jsx
@@ -6,9 +6,24 @@ import WhyChooseUs from '../components/Trust_Us';
 import OurInsights from '../components/insights';
 import ExclusiveStores from '../components/stores';
 import Testimonials from '../components/testimonials';
+import { Helmet } from 'react-helmet-async'
 function Landing_Page() {
   return (
     <div>
+       {/* SEO Meta Tags */}
+       <Helmet>
+        <title>NumLock IT Solutions - Best Laptop Service Center</title>
+        <meta name="description" content="Fast, reliable laptop repair services at NumLock IT Solutions. Book now!" />
+        <meta name="keywords" content="laptop service, NumLock IT Solutions, laptop repair, computer service, IT support" />
+        <meta property="og:title" content="NumLock IT Solutions - Laptop Service Center" />
+        <meta property="og:description" content="Professional laptop repair services you can trust. Book today!" />
+        <meta property="og:image" content="https://numlockitsolutions.co.in/assets/Numlock-CYus4VHR.png" />
+        <meta property="og:url" content="https://numlockitsolutions.co.in/" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="NumLock IT Solutions - Laptop Service Center" />
+        <meta name="twitter:description" content="Professional laptop repair services you can trust. Book today!" />
+        <meta name="twitter:image" content="https://numlockitsolutions.co.in/assets/Numlock-CYus4VHR.png" />
+      </Helmet>
       <Slider/>
       <Hero/>
       <RepairGadgets/>
